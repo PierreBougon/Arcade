0d571c84e26ed9505fb2d5929a71967241c1ccc8
marc brout <marc.brout@epitech.eu>
2017-04-01 16:36:24 +0100

    SpriteGenerator Done

---
+++ games/common/include/SpriteGenerator.hpp
@@ -0,0 +1,36 @@
+//
+// Created by brout_m on 01/04/17.
+//
+
+#ifndef CPP_ARCADE_SPRITEGENERATOR_HPP
+#define CPP_ARCADE_SPRITEGENERATOR_HPP
+
+#include "ISprite.hpp"
+
+namespace arcade
+{
+    class SpriteGenerator : ISprite
+    {
+    public:
+        ~SpriteGenerator() {};
+        SpriteGenerator(std::string const& ascii,
+                        std::string const& path,
+                        std::string const& sprite,
+                        size_t spritesCount,
+                        std::string const& ext);
+
+    public:
+        size_t spritesCount() const;
+        std::string getGraphicPath(size_t pos) const;
+        char getAscii(size_t pos) const;
+
+    private:
+        std::string     Ascii;
+        std::string     Path;
+        std::string     Sprite;
+        size_t          Size;
+        std::string     Ext;
+    };
+}
+
+#endif //CPP_ARCADE_SPRITEGENERATOR_HPP
---
+++ games/common/include/Tile.hpp
@@ -0,0 +1,14 @@
+//
+// Created by brout_m on 01/04/17.
+//
+
+#ifndef CPP_ARCADE_TILE_HPP
+#define CPP_ARCADE_TILE_HPP
+
+
+class Tile {
+
+};
+
+
+#endif //CPP_ARCADE_TILE_HPP
