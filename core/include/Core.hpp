988677c4f406e0a7e2a1670b21456767a5213943
PierreBougon <pierre.bougon@epitech.eu>
2017-04-04 14:36:12 +0200

    Core : Add findLib and FindGame functions to search in the current vector the given lib/game && add game loop with a setable tickrate to allow the game to manage his own speed

--- core/include/Core.hpp
+++ core/include/Core.hpp
@@ -7,7 +7,7 @@
 
 #include <iostream>
 #include <vector>
-#include "IGame.hpp"
+#include "AGame.hpp"
 #include "IGfxLib.hpp"
 #include "Pars.hpp"
 
@@ -16,11 +16,16 @@ namespace arcade
     class Core
     {
     private:
-        std::vector<arcade::IGfxLib *>  tabLib;
-        std::vector<arcade::IGame *>    tabGame;
-        Pars                            pars;
-        std::string                     currentLib;
-        std::string                     currentGame;
+        std::vector<std::unique_ptr<arcade::IGfxLib>>  tabLib;
+        std::vector<std::unique_ptr<arcade::AGame>>    tabGame;
+        Pars                                           pars;
+        bool                                           open;
+
+        // Pointers on current instance of lib & game
+        // They contain raw address of unique_ptr from vectors tabLib & tabGame
+        // DO NOT free those pointers nor delete them
+        IGfxLib                                        *currentLib;
+        AGame                                          *currentGame;
 
     public:
         Core();
@@ -32,14 +37,31 @@ namespace arcade
         /// Init the core of the Arcade
         void init(std::string const &lib);
 
+        bool isOpen() const;
 
 
     private:
         void feedLib();
         void feedGame();
-        int getIndexVec(std::string const &lib, std::vector<std::string> vec);
-        int getIndexLib(std::string const &lib);
-        int getIndexGame(std::string const &game);
+        int getIndexVec(std::string const &lib, std::vector<std::string> vec) const;
+        int getIndexLib(std::string const &lib) const;
+        int getIndexGame(std::string const &game) const;
+
+        /// Find corresponding instance of game or lib
+        AGame   *findGame(const std::string &game);
+        IGfxLib *findLib(const std::string &lib);
+
+        /// Methods relative to the GameState
+        void drawFrame();
+        void initGame();
+        void quitGame();
+        void quitArcade();
+
+        /// Encapsulate the sound calls
+        void playSound();
+
+        /// Encapsulate the networks calls
+        void sendNetwork();
     };
 }
 
--- core/src/Core.cpp
+++ core/src/Core.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <DLLoader.hpp>
+#include <algorithm>
 #include "../include/Core.hpp"
 
 arcade::Core::Core()
@@ -19,27 +20,47 @@ void arcade::Core::init(std::string const &lib)
     pars.FeedVecGame("./games");
     feedLib();
     feedGame();
-    currentLib = lib;
+    currentLib = findLib(lib);
 
     // Basic choice should be decided on a menu
-    currentGame = "./games/snake/snake.so";
+    currentGame = findGame("./games/snake/snake.so");
 }
 
 void arcade::Core::run()
 {
-    int         idx;
-    IGfxLib     *lib;
-    IGame       *game;
-
-    if ((idx = getIndexLib(currentLib)) == -1)
-        return ;
-
-    // Init lib and game for this game loop
-    lib = tabLib[idx];
-    game =  tabGame[getIndexGame(currentGame)];
-
-    lib->updateGUI(game->getGUI());
-    lib->display();
+    while (isOpen())
+    {
+        //events
+        if (currentGame && currentGame->getTick())
+        {
+            currentGame->process();
+            playSound();
+            // SOUNDMANAGER sound currentGame.
+            switch (currentGame->getGameState())
+            {
+                case GameState::QUIT :
+                    quitArcade();
+                    break;
+                case GameState::LOADING :
+                    initGame();
+                    break;
+                case GameState::INGAME :
+                    drawFrame();
+                    break;
+                case GameState::MENU :
+//                    currentLib->updateGUI(currentGame->getGUI());
+                    quitGame();
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            // display arcade menu
+        }
+        currentLib->display();
+    }
 }
 
 void arcade::Core::feedLib()
@@ -60,17 +81,17 @@ void arcade::Core::feedGame()
     }
 }
 
-int arcade::Core::getIndexLib(std::string const &lib)
+int arcade::Core::getIndexLib(std::string const &lib) const
 {
     return (getIndexVec(lib, pars.getVecLib()));
 }
 
-int arcade::Core::getIndexGame(std::string const &game)
+int arcade::Core::getIndexGame(std::string const &game) const
 {
     return (getIndexVec(game, pars.getVecGame()));
 }
 
-int arcade::Core::getIndexVec(std::string const &lib, std::vector<std::string> vec)
+int arcade::Core::getIndexVec(std::string const &lib, std::vector<std::string> vec) const
 {
     int i = 0;
 
@@ -81,4 +102,64 @@ int arcade::Core::getIndexVec(std::string const &lib, std::vector<std::string> v
         ++i;
     }
     return (-1);
+}
+
+arcade::AGame *arcade::Core::findGame(const std::string &game)
+{
+    std::vector<std::unique_ptr<arcade::AGame>>::iterator it =
+            std::find_if(tabGame.begin(), tabGame.end(), [&game](std::string _game){
+                return _game == game;
+            });
+    if (it == tabGame.end())
+        return nullptr;
+    return it->get();
+}
+
+arcade::IGfxLib *arcade::Core::findLib(const std::string &lib)
+{
+    std::vector<std::unique_ptr<arcade::IGfxLib>>::iterator it =
+    std::find_if(tabLib.begin(), tabLib.end(), [&lib](std::string _lib){
+        return _lib == lib;
+    });
+    if (it == tabLib.end())
+        return nullptr;
+    return it->get();
+}
+
+void arcade::Core::drawFrame()
+{
+    currentLib->updateMap(currentGame->getCurrentMap());
+    currentLib->updateGUI(currentGame->getGUI());
+}
+
+void arcade::Core::initGame()
+{
+    currentLib->loadSounds(currentGame->getSoundsToLoad());
+    currentLib->loadSprites(currentGame->getSpritesToLoad());
+}
+
+bool arcade::Core::isOpen() const
+{
+    return open;
+}
+
+void arcade::Core::quitGame()
+{
+    currentGame = nullptr;
+}
+
+void arcade::Core::quitArcade()
+{
+    // TODO check new implementation
+    open = false;
+}
+
+void arcade::Core::playSound()
+{
+    currentLib->soundControl(currentGame->getSoundsToPlay());
+}
+
+void arcade::Core::sendNetwork()
+{
+    //currentGame->getNetworkToSend()
 }
