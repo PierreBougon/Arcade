91dde064917192a03ab4ebd9511cb87376ad1d57
PierreBougon <pierre.bougon@epitech.eu>
2017-04-09 06:40:10 +0200

    Makefile colors and rules : updated

--- mk/colors.mk
+++ mk/colors.mk
@@ -1,9 +1,9 @@
 # Clear screen
-CLEAR		= \033[0m
+CLEAR   	= \033[0m
 
 ## Colors
 BLUE    	= \033[1;34m
 BOLD_WHITE   	= \033[1;37m
-WHITE   	= \033[0m
+WHITE   	= $(CLEAR)
 RED     	= \033[1;31m
 GREEN   	= \033[1;32m
--- mk/rules.mk
+++ mk/rules.mk
@@ -1,10 +1,10 @@
 ## Generic rules for Makefiles
 
 $(NAME):	$(OBJ)
-		@$(ECHO) "$(BLUE) Compiled with : $(BOLD_WHITE)$(CXX)$(CLEAR)"
+		@$(ECHO) "\n$(BLUE) Compiled with : $(BOLD_WHITE)$(CXX)$(CLEAR)"
 		@$(ECHO) "$(BLUE) Flags used \t  : $(BOLD_WHITE)$(CXXFLAGS)$(CLEAR)"
 		@$(ECHO) "$(BLUE) Dependencies  :$(BOLD_WHITE)$(LDFLAGS)$(CLEAR)"
-		@$(ECHO) "$(BLUE) == $(BOLD_WHITE) $(PROJECT_NAME)  Compiled $(BLUE) == $(CLEAR)\n"
+		@$(ECHO) "$(BLUE) == $(BOLD_WHITE) $(PROJECT_NAME)  Compiled $(BLUE) == $(CLEAR)\n\n"
 		@$(CXX) $(OBJ) -o $(NAME) $(INC) $(LDFLAGS)
 		@$(eval PROJECT_NAME=)
 		@$(eval LDFLAGS=)
@@ -14,17 +14,18 @@ $(NAME):	$(OBJ)
 all:		$(NAME)
 
 clean:
-		@$(ECHO) "$(BOLD_WHITE) [$(RED)RM$(BOLD_WHITE)] $(OBJ)$(CLEAR)"
+		@$(ECHO) "$(BOLD_WHITE) [$(RED)RM$(BOLD_WHITE)] $(NAME) Obejcts files$(CLEAR)"
 		@$(RM) $(OBJ)
 
 fclean:     	clean
-		@$(ECHO) "$(BOLD_WHITE) [$(RED)RM$(BOLD_WHITE)] $(NAME)$(CLEAR)\n"
+		@$(ECHO) "$(BOLD_WHITE) [$(RED)RM$(BOLD_WHITE)] $(NAME) Binary file$(CLEAR)\n"
 		@$(RM) $(NAME)
 
 re:		fclean all
 
-.cpp.o:ifeq ($(STATIC), YES)
+.cpp.o:
+#ifeq ($(STATIC), YES)
 		@$(CXX) $(CXXFLAGS) $(INC) $(LDFLAGS) -c $< -o $@
 		@$(ECHO) "$(BOLD_WHITE) [$(GREEN)OK$(BOLD_WHITE)] Compiled "$<"$(CLEAR)"
-
+#endif
 .PHONY:		all clean fclean re
